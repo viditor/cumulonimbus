47830fe794f5dbcd5b92ddff7cfec4f83995eb21
Andrew McPherson <andrewmcp333@gmail.com>
2014-07-17 08:31:17 -0400

    Revised the system to save to a database.

--- router.v1.js
+++ router.v1.js
@@ -1,65 +1,90 @@
 router = module.exports = require("express").Router();
 
+var ARCHIVED_ASSETS = __dirname + "/archived_assets";
+
 var execute = require("child_process").exec;
-var database = {/*not actually the database*/};
+var database = require("mongojs")("viditorcloud", ["assets"]);
+
+database.assets.remove({});
 
 router.get("/youtube", function(request, response)
 {
-	response.send(database);
+	database.assets.find({}).sort({}, function(error, assets)
+	{
+		response.send(assets);
+	});
 });
 
 router.get("/youtube/:ytid.:ext", function(request, response)
 {
 	var ytid = request.params.ytid;
 	var ext = request.params.ext;
-	console.log(ext);
 	
 	if(ext != "mp4" && ext != "webm" && ext != "ogv" && ext != "flv")
 	{
 		return response.send(404, {error: "Not a supported video extension."});
 	}
 	
-	if(!database[ytid])
+	database.assets.findOne({ytid: ytid}, function(error, asset)
 	{
-		return response.send(404, {error: "Unable to access the video."});
-	}
-	
-	response.sendfile("./archived_assets/" + ytid + "." + ext);
+		if(asset)
+		{
+			return response.sendfile(ARCHIVED_ASSETS + "/" + ytid + "." + ext);
+		}
+		else
+		{
+			return response.send(404, {error: "Unable to access the asset."});
+		}
+		
+		//todo: handle errors in connecting to the database.
+	});
 });
 
 router.get("/youtube/:ytid", function(request, response)
 {
 	var ytid = request.params.ytid;
 	
-	if(database[ytid])
+	database.assets.findOne({ytid: ytid}, function(error, asset)
 	{
-		response.send(200, database[ytid]);
-	}
-	else
-	{
-		response.send(404, "Not Authorized");
-	}
+		if(asset)
+		{
+			return response.send(200, asset);
+		}
+		else
+		{
+			return response.send(404, {error: "Unable to access the asset."});
+		}
+		
+		//todo: handle errors in connecting to the database.
+	});
 });
 
 router.post("/youtube/:ytid", function(request, response)
 {
 	var ytid = request.params.ytid;
 	
-	if(!database[ytid])
+	database.assets.findOne({ytid: ytid}, function(error, asset)
 	{
-		var yturl = "http://www.youtube.com/watch?v=" + ytid;
-		var path = "archived_assets/" + ytid + ".flv";
-		
-		database[ytid] = {status: "downloading"};
-		response.send(200, database[ytid]);
-		
-		execute("ytdl " + yturl + " > " + path, function()
+		if(asset)
 		{
-			database[ytid].status = "archived";
-		});
-	}
-	else
-	{
-		response.send(404, "Video already downloaded");
-	}
+			return response.send(200, asset);
+		}
+		else
+		{
+			var asset = {ytid: ytid, status: "downloading"};
+			database.assets.insert(asset, {}, function(error, asset)
+			{
+				var yturl = "http://www.youtube.com/watch?v=" + ytid;
+				var filepath = ARCHIVED_ASSETS + "/" + ytid + ".flv";
+				execute("ytdl " + yturl + " > " + filepath, function()
+				{
+					database.assets.update({ytid: ytid}, {$set: {status: "archived"}});
+				});
+				
+				return response.send(200, asset); //asset?!
+			})
+		}
+		
+		//todo: handle errors in connecting to the database.
+	});
 });
