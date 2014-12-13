0f76716a7e2a344b529b71b3a76288e636621fe5
Andrew McPherson <andrewmcp333@gmail.com>
2014-12-13 07:31:40 -0500

    Emptied the repository so we can start again.

--- nmailer.js
+++
@@ -1,6 +0,0 @@
-var nodemailer = require("nodemailer");
-
-Asset = require("./asset.schema.js");
-Asset.remove({}, function(error) {});
-
-var transport = nodemailer.createTransport("SMTP", {service: "gmail", auth: require("./volatile_data/gmail.account.js")});
