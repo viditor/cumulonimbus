6b8bc85fd458931f7f79cb557385f434e5a82cb4
tzorca <tzorca@users.noreply.github.com>
2015-01-21 17:48:41 -0500

    Saved asset entry in db when downloading youtube video.

--- source/youtube.downloader.js
+++ source/youtube.downloader.js
@@ -1,5 +1,6 @@
 var ytdl = require('ytdl-core');
 var fs = require('fs');
+var db = require("mongojs").connect("mongodb://localhost", ['assets']);
 
 module.exports =
 {
@@ -7,5 +8,11 @@ module.exports =
     {
         ytdl("http://www.youtube.com/watch?v=" + ytid)
             .pipe(fs.createWriteStream(ytid + ".flv"));
+
+        db.assets.save
+        ({
+            "ytid": ytid,
+            "downloadTime": Date.now()
+        });
     }
 }
