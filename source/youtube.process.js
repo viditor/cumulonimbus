6b702fb24c07488afce5fbb9d9f4ebee05522fb9
Andrew McPherson <andrewmcp333@gmail.com>
2015-01-23 12:30:04 -0500

    Switched to bluebird as our promise library.

--- source/youtube.process.js
+++ source/youtube.process.js
@@ -1,40 +1,39 @@
-var q = require("q");
 var fs = require("fs");
 var ytdl = require("ytdl-core");
+var Bluebird = require("bluebird");
 
 module.exports =
 {
     download: function(ytid)
     {
-        var deferred = q.defer();
-        
-        var filename = ytid + ".flv";
-        var yturl = "http://www.youtube.com/watch?v=" + ytid;
+        return new Bluebird(function(resolve, reject)
+        {
+            var filename = ytid + ".flv";
+            var yturl = "http://www.youtube.com/watch?v=" + ytid;
 
-        var process = ytdl(yturl);
-        
-        /*process.on("data", function(data)
-        {
-            console.log(data);
-        });*/
-        
-        /*process.on("info", function(info)
-        {
-            console.log(info);
-        });*/
-        
-        process.on("error", function(error)
-        {
-            deferred.reject(error);
+            var process = ytdl(yturl);
+            
+            /*process.on("data", function(data)
+            {
+                console.log(data);
+            });*/
+            
+            /*process.on("info", function(info)
+            {
+                console.log(info);
+            });*/
+            
+            process.on("error", function(error)
+            {
+                reject(error);
+            });
+            
+            process.on("end", function()
+            {
+                resolve(filename);
+            });
+            
+            process.pipe(fs.createWriteStream(filename));
         });
-        
-        process.on("end", function()
-        {
-            deferred.resolve(filename);
-        });
-        
-        process.pipe(fs.createWriteStream(filename));
-        
-        return deferred.promise;
     }
 }
