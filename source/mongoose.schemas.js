1620db0978701aea8e7e672cc1f2c1499b417cf1
tzorca <tzorca@users.noreply.github.com>
2015-02-04 22:30:20 -0500

    Replaced exports with plain calls in mongoose.schemas.

--- source/mongoose.schemas.js
+++ source/mongoose.schemas.js
@@ -1,46 +1,40 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
-module.exports =
-{
-    loadSchemas: function()
+var assetSchema = new Schema
+({
+    ytid: String,
+    files:
     {
-        var assetSchema = new Schema
-        ({
-            ytid: String,
-            files:
-            {
-                original: String,
-                mp4: String,
-                webm: String, 
-                ogv: String
-            },
-            dates:
-            {
-                created: Number,
-                touched: Number
-            }
-        });
-
-        assetSchema.methods.touch = function (_ytid, callback)
-        {
-            var query =
-            {
-                ytid: _ytid
-            };
-
-            var update =
-            {
-                dates:
-                {
-                    touched: Date.now()
-                }
-            };
-
-            this.model('Asset').findOneAndUpdate(query, update, {}, callback);
-        }
-
-        mongoose.model("Asset", assetSchema);
+        original: String,
+        mp4: String,
+        webm: String, 
+        ogv: String
+    },
+    dates:
+    {
+        created: Number,
+        touched: Number
     }
+});
+
+assetSchema.methods.touch = function (_ytid, callback)
+{
+    var query =
+    {
+        ytid: _ytid
+    };
+
+    var update =
+    {
+        dates:
+        {
+            touched: Date.now()
+        }
+    };
+
+    this.model("Asset").findOneAndUpdate(query, update, {}, callback);
 }
+
+mongoose.model("Asset", assetSchema);
