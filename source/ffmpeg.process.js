4e906f24bbc07b78edd79428573b9ce9ae501290
Andrew McPherson <andrewmcp333@gmail.com>
2015-02-13 09:52:43 -0500

    Commented the methods for transcoding videos.

--- source/ffmpeg.process.js
+++ source/ffmpeg.process.js
@@ -2,6 +2,37 @@
 var Bluebird = require("bluebird");
 var ffmpeg = require("fluent-ffmpeg");
 
+/**
+ * TRANSCODE: decodes and encodes a
+ * video in a different format.
+ *
+ * INPUT
+ * - directory (String)
+ * Where the video is pulled, and where
+ * the transcoded file will be pushed.
+ * - file_handle (String)
+ * The name of the video *without* any
+ * extensions; "foo" not "foo.mp4".
+ * - input_format (String)
+ * The format of the video before it is
+ * transcoded, like "flv" or "avi".
+ * - output_format (String)
+ * The format of the video after it has
+ * been transcoded, like "mp4" or "ogv".
+ * - video_codec (String)(Optional)
+ * The name of a video codec that is
+ * supported by FFMPEG.
+ * - audio_codec (String)(Optional)
+ * The name of an audio codec that is
+ * supported by FFMPEG.
+ *
+ * OUTPUT
+ * - A promise that will resolve when
+ * the video has been transcoded, and
+ * returns a filepath to the newly
+ * transcoded video.
+ */
+ 
 module.exports.transcode = function(directory, file_handle,
                                     input_format, output_format,
                                     video_codec, audio_codec)
@@ -30,10 +61,10 @@ module.exports.transcode = function(directory, file_handle,
             reject(error);
         });
         
-        //transcoding.on("progress", function(progress)
-        //{
-        //    console.log(output, progress.frames);
-        //});
+         * transcoding.on("progress", function(progress)
+         * {
+         *     console.log(output, progress.frames);
+         * });
         
         transcoding.on("end", function()
         {
@@ -44,28 +75,47 @@ module.exports.transcode = function(directory, file_handle,
     })
 }
 
-module.exports.splice = function(a, b)
+/**
+ * WEBTRANSCODE: decodes and encodes
+ * video into the different formats
+ * needed for the web.
+ *
+ * INPUT
+ * - directory (String)
+ * Where the video is pulled, and where
+ * the transcoded file will be pushed.
+ * - file_handle (String)
+ * The name of the video *without* any
+ * extensions; "foo" not "foo.mp4".
+ * - input_format (String)
+ * The format of the video before it is
+ * transcoded, like "flv" or "avi".
+ * - output_format (String)
+ * The format of the video after it has
+ * been transcoded, like "mp4" or "ogv".
+ * - video_codec (String)(Optional)
+ * The name of a video codec that is
+ * supported by FFMPEG.
+ * - audio_codec (String)(Optional)
+ * The name of an audio codec that is
+ * supported by FFMPEG.
+ *
+ * OUTPUT
+ * - A promise that will resolve when
+ * the video has been transcoded into
+ * three different formats, including
+ * WEBM, OGV and MP4, and returns an
+ * array of filepaths to the newly
+ * transcoded videos.
+ */
+ 
+module.exports.webtranscode = function(directory, file_handle)
 {
-    ffmpeg()
-    .input(a)
-    .on("error", function(error, stdout, stderr)
-    {
-      console.log(stderr);
-    })
-    .on("end", function()
-    {
-        console.log("Splicing finished!");
-    })
-    .mergeToFile("../assets/output.avi", "../temp/");
+    var transcodings = [
+        module.exports.transcode(directory, file_handle, "flv", "mp4", "libx264"),
+        module.exports.transcode(directory, file_handle, "flv", "webm", "libvpx", "libvorbis"),
+        module.exports.transcode(directory, file_handle, "flv", "ogv", "libtheora", "libvorbis")
+    ];
+    
+    return Bluebird.settle(transcodings)
 }
-
-module.exports.transcode("../assets/", "kfchvCyHmsc", "flv", "mp4", "libx264").then(function()
-{
-    console.log("Done!")
-})
-
-//module.exports.splice("../assets/kfchvCyHmsc.flv",
-//                      "../assets/UiyDmqO59QE.flv")
-
-//require("./youtube.process").download("kfchvCyHmsc")
-//require("./youtube.process").download("UiyDmqO59QE")
