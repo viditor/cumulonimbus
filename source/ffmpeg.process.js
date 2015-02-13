9bb901d3d233ec2f22b844e260bf5533d2296eeb
Andrew McPherson <andrewmcp333@gmail.com>
2015-02-13 08:54:34 -0500

    Set up a module for handling the ffmpeg stuff.

---
+++ source/ffmpeg.process.js
@@ -0,0 +1,12 @@
+var ffmpeg = require("fluent-ffmpeg");
+
+module.exports.splice = function(a, b)
+{
+    console.log(a, b);
+}
+
+module.exports.splice("../assets/kfchvCyHmsc.flv",
+                      "../assets/UiyDmqO59QE.flv")
+
+//require("./youtube.process").download("kfchvCyHmsc")
+//require("./youtube.process").download("UiyDmqO59QE")
