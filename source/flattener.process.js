f56a076b8758e006918720ae431029a9a23d2480
tzorca <tzorca@users.noreply.github.com>
2015-02-22 15:22:30 -0500

    Coded flattener to handle first four test cases.

--- source/flattener.process.js
+++ source/flattener.process.js
@@ -1,5 +1,3 @@
-var Bluebird = require("bluebird");
-
 /**
  * flatten: Handles edge cases in a clip list to prepare it for rendering. Flattens multiple tracks into a single track and replaces spaces with seconds of blackness.
  *
@@ -8,26 +6,46 @@ var Bluebird = require("bluebird");
  * A JSON array of clip metadata. Should be sorted by tic in ascending order. 
  *
  * OUTPUT
- * - A promise that will resolve when the flattened array of clips has been completed, and will return  the flattened array.
+ * - A promise that will resolve when the flattened array of clips has been completed, and will return the flattened array.
  * 
  */
 module.exports.flatten = function(ticSortedClips)
 {
-    return new Bluebird(function(resolve, reject)
+    var previousEndTick = 0;
+    for (var i = 0; i < ticSortedClips.length; i++)
     {
-        // (Placeholder)
-        resolve(ticSortedClips);
-
-        // transcoding.on("error", function(error)
-        // {
-        //     reject(error);
-        // });
+        var clip = ticSortedClips[i];
         
-        // transcoding.on("end", function()
-        // {
-        //     resolve(output_file);
-        // });
+        // For now, naively put all clips on track 0
+        clip.track = 0;
 
-        // process.pipe(fs.createWriteStream(file));
-    })
+        if (clip.tick > previousEndTick)
+        {
+            // Insert blackness where there is a gap between clips
+            var blacknessLength = clip.tick - previousEndTick;
+            ticSortedClips.splice(i, 0, createBlackness(previousEndTick, blacknessLength, clip.project_id));
+            i++;
+        }
+        previousEndTick = clip.tick + clip.length;
+    }
+
+    return ticSortedClips;
+}
+
+// Creates a clip of blackness for the specified project at the specified tick with the specified length
+function createBlackness(tick, length, project_id)
+{
+    return {
+        "_id": "blackness-placeholder",
+        "asset_id": "blackness-placeholder",
+        "project_id": project_id,
+        "trim":
+        {
+            "left": 0,
+            "right": 0
+        },
+        "tick": tick,
+        "length": length,
+        "track": 0
+    };
 }
