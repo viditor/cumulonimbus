40230860113c043e2f3c2ef2bbae2fe43f4e3609
John Angele <jtangele+github@gmail.com>
2015-02-23 19:18:09 -0500

    added mailKey.js sample file.

---
+++ mailKeys.js
@@ -0,0 +1,8 @@
+
+
+// Thu Feb  5 19:35:48 2015 | 1423182948
+// variables to export to keep private from general code
+
+exports.mailAddress   = 'GMAIL ADDRESS';
+exports.cryptPassword = 'ENCRYPTION PASSWORD';
+exports.hashValue     = 'ENCRYPTED GMAIL ADDRESS';
