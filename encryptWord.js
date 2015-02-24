38935741550b2232b7d2124658cdcc3ee10b6c68
j--a <jtangele+github@gmail.com>
2015-02-24 10:47:14 -0500

    Update encryptWord.js

--- encryptWord.js
+++ encryptWord.js
@@ -1,9 +1,8 @@
-#!/usr/bin/env node
 
 // Fri Jan 23 19:38:20 2015 | 1422059900
 // Tool to produce encryped email password for use in mailKey.js
 // Replace ENCRYPTION PASSWORD and EMAIL PASSWORD
-// with the respective values for those uses
+// with the respective values for those uses.
 
 var crypto = require('crypto'),
 algorithm = 'aes-256-ctr',
