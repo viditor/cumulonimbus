9a37942de86650bd845f432d5816cd5d2ee44f4d
Andrew McPherson <andrewmcp333@gmail.com>
2015-02-26 07:46:21 -0500

    Defined a store for handling all the assets.

--- server.js
+++ server.js
@@ -2,12 +2,27 @@ var http = require("http");
 var express = require("express");
 var socketio = require("socket.io");
 
+var AssetStore = require("./AssetStore.js");
+
+/////////////////
+//Initializing//
+///////////////
+
+var MyAssets = new AssetStore();
+
 //require("./source/mongoose.connection");
 //require("./source/mongoose.schemas");
 
 application = express();
 application.use("/v2", require("./source/router.js"));
 application.use("/greet", require("./source/greet.router.js"));
+
+var asset_id = 1;
+application["get"]("/debug", function(request, response) {
+    MyAssets.addAsset(asset_id++);
+    response.status(200).send("OK");
+});
+
 application["all"]("*", function(request, response)
 {
     response.status(404).send("put error message here");
@@ -31,5 +46,16 @@ server.listen(port, function()
 var io = socketio(server);
 io.on("connection", function(socket)
 {
-    socket.emit("Establish a connection");
+    var assets = MyAssets.getAllAssets()
+    for(var video_id in assets)
+    {
+        var asset = assets[asset_id];
+        socket.emit("add asset", asset);
+    }
+
+    MyAssets.on("add asset", function(asset)
+    {
+        socket.emit("add asset", asset);
+    });
 });
+
