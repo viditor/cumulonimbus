e4b902b0a2e923a52cdcdae395e637a433cf1566
tzorca <tzorca@users.noreply.github.com>
2015-03-14 10:28:51 -0400

    Reverted commenting out other spec files.

--- specs/mongoose.schemas.spec.js
+++ specs/mongoose.schemas.spec.js
@@ -1,51 +1,51 @@
-// var mongoose = require("mongoose");
+var mongoose = require("mongoose");
 
-// xdescribe("Asset schema", function()
-// {
-//     it("allows creation of new assets", function(done)
-//     {
-//         require("../source/mongoose.connection");
-//         require("../source/mongoose.schemas");
+xdescribe("Asset schema", function()
+{
+    it("allows creation of new assets", function(done)
+    {
+        require("../source/mongoose.connection");
+        require("../source/mongoose.schemas");
         
-//         // Create dummy asset
-//         mongoose.model("Asset").create
-//         ({
-//             ytid: "TEST",
-//             dates:
-//             {
-//                 created: Date.now(),
-//                 touched: 0
-//             }
-//         })
-//         .then(function(error, asset)
-//         {
-//             done();
-//         });
-//     }, 250);
+        // Create dummy asset
+        mongoose.model("Asset").create
+        ({
+            ytid: "TEST",
+            dates:
+            {
+                created: Date.now(),
+                touched: 0
+            }
+        })
+        .then(function(error, asset)
+        {
+            done();
+        });
+    }, 250);
 
 
-//     it("updates the 'touched' date of the asset", function(done)
-//     {
-//         var promiseA = mongoose.model("Asset").findOne({ytid: "TEST"}).exec();
+    it("updates the 'touched' date of the asset", function(done)
+    {
+        var promiseA = mongoose.model("Asset").findOne({ytid: "TEST"}).exec();
         
-//         promiseA.then(function(errorA, assetA)
-//         {
-//             expect(errorA).toBeUndefined();
-//             expect(assetA).toBeDefined();
+        promiseA.then(function(errorA, assetA)
+        {
+            expect(errorA).toBeUndefined();
+            expect(assetA).toBeDefined();
 
-//             assetA.touch(function()
-//             {
-//                 var promiseB = mongoose.model("Asset").findOne({ytid: "TEST"}).exec();
-//                 promiseB.then(function(errorB, assetB)
-//                 {
-//                     expect(errorB).toBeUndefined();
-//                     expect(assetB).toBeDefined();
-//                     expect(assetB.dates.touched).not.toEqual(0);
+            assetA.touch(function()
+            {
+                var promiseB = mongoose.model("Asset").findOne({ytid: "TEST"}).exec();
+                promiseB.then(function(errorB, assetB)
+                {
+                    expect(errorB).toBeUndefined();
+                    expect(assetB).toBeDefined();
+                    expect(assetB.dates.touched).not.toEqual(0);
 
-//                     done();
-//                 });
-//             });
-//         });
-//     }, 250);
+                    done();
+                });
+            });
+        });
+    }, 250);
 
-// });
+});
