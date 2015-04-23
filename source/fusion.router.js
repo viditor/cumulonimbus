ee1d648e138018e07a5f72b6de6972522f995ec2
Andrew McPherson <andrewmcp333@gmail.com>
2015-04-22 20:35:30 -0400

    Connected the fusion router to the fusion module.

--- source/fusion.router.js
+++ source/fusion.router.js
@@ -1,13 +1,17 @@
 var express = require("express")
 var bodyparser = require("body-parser")
+var fusion = require("fusion")
 
 var FusionRouter = express.Router()
 
 FusionRouter["post"]("/", bodyparser.json(), function(request, response) {
-	console.log(request.body)
-	response.send({
-		"format": "flv"
-	})
+	var protovideo = request.body
+    fusion(protovideo).then(function(video) {
+        response.status(200).send(video)
+    }).catch(function(error) {
+        console.log(error)
+        response.status(400).send(error)
+    })
 })
 
 module.exports = FusionRouter
